9343cee47196af019961745833f29b4bbaec799e
Degisew <jegisew21@gmail.com>
2025-07-21 14:42:57 +0300

    fix: add CORS middleware

--- backend/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI, UploadFile
-
-app = FastAPI()
-
-
-@app.post("/upload")
-def upload_document(file: UploadFile):
-    print(file.filename)
--- main.py
+++ main.py
@@ -2,11 +2,26 @@ from typing import Any
 from backend.chat import process_query
 from backend.ingest import ingest_embeddings
 from fastapi import BackgroundTasks, FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from backend.schemas import ChatSchema
 from backend.utils import save_document
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/upload")
